16cef5705fb8719a0cdcdfd2659bb0d877664a7c
SElshal1 <selshals508@gmail.com>
2024-04-06 14:50:27 -0700

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,12 @@
 # Automating my Tasks using Puppet
 
 file { '/etc/ssh/ssh_config':
-  ensure  => present,
+	ensure  => present,
+
 content => "
-    # SSH client configuration
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
+	#SSH client configuration
+	Host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
 }
