e2dcc32747607c4bddbf411c34f6f5ebff0065c3
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-04-22 02:25:31 +0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-listed for a given subreddit."""
+""" Queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit. """
+
 import requests
+headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/Certifieddonnie)"}
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code == 200:
+        for children in request.json().get("data").get("children"):
+            print(children.get("data").get("title"))
+
+    else:
+        print(None)
