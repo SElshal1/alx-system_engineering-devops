4a5f924a4defa2450968d0778159bd4f11a7664a
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-04-22 02:15:27 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 """A function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-listed for a given subreddit."""
+returns the number of subscribers"""
 import requests
 
 
-def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
-        print("None")
-        return
+        return 0
     results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    return results.get("subscribers")
