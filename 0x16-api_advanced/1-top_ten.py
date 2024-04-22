ea840891914a71c24f6d7b2b48f08388960634d8
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-04-22 02:15:47 +0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" module 1-top_ten.py """
+"""A function that queries the Reddit API and
+prints the titles of the first 10 hot posts
+listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top 10 hot posts for a given subreddit"""
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Test"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        for i in range(10):
-            print(response.json().get("data").get("children")[i]
-                  .get("data").get("title"))
-    else:
-        print(None)
+    """prints the titles of the first 10 hot posts listed"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
