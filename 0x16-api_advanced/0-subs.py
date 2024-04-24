845449a2eea81af1046daa74f646ccc826ca2ce2
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-04-24 16:46:07 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,21 @@
-
 #!/usr/bin/python3
-"""
-100-main
-"""
-import sys
+"""0-subs
 
-if __name__ == '__main__':
-    count_words = __import__('100-count').count_words
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+Run ./0-main.py programming or ./0-main.py this_is_a_fake_subreddit for testing
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "0-subs.py"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
     else:
-        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+        return 0
